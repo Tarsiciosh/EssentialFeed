2cf6a9c43e02792a3fc541358ac122b6e4d5fde5
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2022-10-07 17:11:41 -0300

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Tarsicio Spraggon Hernández on 07/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
