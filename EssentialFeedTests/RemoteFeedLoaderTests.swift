45e2aaa6cadb5d1dce0ed2adaf1a703c8bee3590
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2022-10-11 19:57:20 -0300

    give a URL instead of hardcoding it in the RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -2,13 +2,15 @@ import XCTest
 
 class RemoteFeedLoader {
     var client: HTTPClient
+    var url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "http://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -27,22 +29,24 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
     
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         //ARRANGE (GIVEN)
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         //ACT (WHEN)
         sut.load()
         
         //ASSERT (THEN)
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
 
@@ -53,17 +57,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
 /*
 To test using a singleton:
+- make "shared" in the singleton class be a variable
+- move the test logic form the RemoteFeedLoader to the HTTPClient (capturing the url)
+- to do that then we create a get method in the HTTPClient and capture the url there
+- create a spy subclassing the singleton class with old things needed to the test and then change the singleton shared with the spy
+- remove the private initializer to let the spy initialize
 
-1) make "shared" in the singleton class be a variable
-2) move the test logic form the RemoteFeedLoader to the HTTPClient (capturing the url)
-3) to do that then we create a get method in the HTTPClient and capture the url there
-4) create a spy subclassing the singleton class with old things needed to the test and then change the singleton shared with the spy
-5) remove the private initializer to let the spy initialize
-
- 
 To start removing the singleton
-1) inject the client into the RemoteFeedLoader (constructor injection)
-2) remove the shared instance of the singleton class
-3) transform the abstract class HTTPClient to a protocol
-4) then the spy instead of inheriting the HTTPClient it implements the HTTPClient protocol
+- inject the client into the RemoteFeedLoader (constructor injection)
+- remove the shared instance of the singleton class
+- transform the abstract class HTTPClient to a protocol
+- then the spy instead of inheriting the HTTPClient it implements the HTTPClient protocol
+ 
+Test the URL is ok:
+- assert if the url is the same as the one given to the RemoteFeedLoader (change to accept a URL)
 */
