9995600ad0d2fcab08704cd7ee1b12362c4c157a
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2022-10-25 20:31:17 -0300

    Delivers invalidData error on 200 HTTP response with invalid json

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -61,6 +61,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var captureErrors = [RemoteFeedLoader.Error]()
+        sut.load { captureErrors.append($0) }
+        
+        let invalidJSON = Data("invalid".utf8)
+ 
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        XCTAssertEqual(captureErrors, [.invalidData])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -86,14 +98,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
@@ -131,5 +143,11 @@ Check response:
 - create specific types of Error in the RemoteFeedLoader
  
 - create an array of errors and compare this array
+
+## A Classicist TDD Approach (No Mocking) to Mapping JSON with Decodable + Domain-Specific Models
+
+-
+
+ 
 */
 
