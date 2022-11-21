2019351b87929a241f583d3bd90a5e6c7e8a731b
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2022-11-21 17:23:10 -0300

    save command requests cache deletion

--- EssentialFeedTests/Feed Cache/CahceFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CahceFeedUseCaseTests.swift
@@ -1,13 +1,24 @@
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore //colaborator
+    
     init (store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CahceFeedUseCaseTests: XCTestCase {
@@ -17,5 +28,29 @@ final class CahceFeedUseCaseTests: XCTestCase {
         
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    //MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        FeedItem(
+            id: UUID(),
+            description: "any",
+            location: "any",
+            imageURL: anyURL()
+        )
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
     }
 }
--- README.md
+++ README.md
@@ -97,4 +97,5 @@ let request = URLRequest(url: url, cachePolicy: .returnCacheDataDontLoad, timeou
 #### there is going to be an intermediate object (LocalFeedLoader - sut) that will have a store (FeedStore) to communicate to the actual framework responsible of storing the data (could be a db, file system etc.)
 
 #### 1. the localFeedLoader (sut) does not invoke the delete command to the store upon creation (deleteChaedFeedCallCount = 0)
-#### 2.  
+#### 2. save command on sut request cache deletion (save command receives also the items to be saved) (create helper methods for the items) 
+#### 3. 
