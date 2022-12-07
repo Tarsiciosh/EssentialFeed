df0cfc830c395f19f7d708a36166a65fae95b35a
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2022-12-07 15:05:21 -0300

    insert delivers error on insertion error (invalid store URL)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -52,11 +52,15 @@ class CodableFeedStore {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -138,6 +142,15 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
     }
     
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let insertionError = insert((feed, timestamp), to: sut)
+        XCTAssertNotNil(insertionError)
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil ,file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -152,7 +165,6 @@ final class CodableFeedStoreTests: XCTestCase {
         var insertionError: Error?
         let exp = expectation(description: "Wait for cache insertion")
         sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
-            XCTAssertNil(receivedInsertionError, "Expected feed to be inserted successfully")
             insertionError = receivedInsertionError
             exp.fulfill()
         }
