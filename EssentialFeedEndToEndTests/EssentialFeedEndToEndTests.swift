3e1f52a10e46b43ebf4e104ace395e725142b6ac
Tar <tarsiciosh@gmail.com>
2023-01-26 18:00:43 -0300

    nest `LoadFeedResult` into the `FeedLoader` protocol as `FeedLoader.Result` since they're closely related

--- EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
+++ EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
@@ -26,7 +26,7 @@ final class EssentialFeedEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
@@ -36,7 +36,7 @@ final class EssentialFeedEndToEndTests: XCTestCase {
         
         let exp = expectation(description: "wait for load completion")
         
-        var recievedResult: LoadFeedResult?
+        var recievedResult: FeedLoader.Result?
         loader.load { result in
             recievedResult = result
             exp.fulfill()
