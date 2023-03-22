6115b45b9b0be1eadca2650f82aef558b2404e2e
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2023-03-22 19:03:44 -0300

    hide feed load error on reload

--- EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -9,7 +9,7 @@ protocol FeedView {
 }
 
 struct FeedErrorViewModel {
-     let message: String
+     let message: String?
  }
 
  protocol FeedErrorView {
@@ -42,6 +42,7 @@ final class FeedPresenter {
     }
     
     func didStartLoadingFeed() {
+        errorView.display(FeedErrorViewModel(message: nil))
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
     
